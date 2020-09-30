caa05406118121101a0bf2592a2582b88fdf37a1
Sarah <sarahmichelleduong@gmail.com>
2020-09-30 17:09:08 -0400

    Updates

---
+++ precache-manifest.ce2f5e6cf18ac4f5004fb986aa627df9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba22e6f7d983460b372ab977fb1af097",
+    "url": "/sarahDuongProjectFive/index.html"
+  },
+  {
+    "revision": "20bde9529c445e807376",
+    "url": "/sarahDuongProjectFive/static/css/main.aea42118.chunk.css"
+  },
+  {
+    "revision": "a2ef4d7499575a17e971",
+    "url": "/sarahDuongProjectFive/static/js/2.f230a4e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sarahDuongProjectFive/static/js/2.f230a4e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20bde9529c445e807376",
+    "url": "/sarahDuongProjectFive/static/js/main.c44e2509.chunk.js"
+  },
+  {
+    "revision": "5ea51ebd437cc44b9302",
+    "url": "/sarahDuongProjectFive/static/js/runtime-main.e31492e8.js"
+  }
+]);
